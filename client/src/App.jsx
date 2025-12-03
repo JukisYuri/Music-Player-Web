752ece415139c0733ffb4b313d10536c272279bc
JukisYuri <yurichangirl752@gmail.com>
2025-12-03 18:36:56 +0700

    Tạm thời làm trang Profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,18 @@ import { Login } from './pages/Login.jsx'
 import { Register } from './pages/Register.jsx'
 import { ForgotPassword } from './pages/Forgot-Password.jsx'
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { Profile } from './pages/Profile.jsx';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/" element={<Navigate to="/profile" replace />} />
       
       {/* Định nghĩa đường dẫn */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   )
 }
