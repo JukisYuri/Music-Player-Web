dc3b96a3e1a9f329b07ac30c160f286dfef83192
JukisYuri <yurichangirl752@gmail.com>
2025-12-17 15:11:59 +0700

    thêm i18n lib, base cho vi và en languages

--- client/src/pages/Setting.jsx
+++ client/src/pages/Setting.jsx
@@ -3,14 +3,21 @@ import { Sidebar } from '../components/sidebar.jsx';
 import { HeaderBar } from '../components/header_bar.jsx';
 import { PlayerBar } from '../components/player_bar.jsx';
 import { LogoutConfirmModal } from '../components/logout_confirm_modal.jsx';
+import { useTranslation } from 'react-i18next';
+import i18n from "../i18n.js";
 
 export function Setting() {
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const { t } = useTranslation();
     const [selectedLanguage, setSelectedLanguage] = useState('vi');
 
     const handleLanguageChange = (e) => {
-        setSelectedLanguage(e.target.value);
+        const language = e.target.value;
+        setSelectedLanguage(language);
+        i18n.changeLanguage(language);
     };
+
+
 
     return (
         <>
@@ -24,13 +31,13 @@ export function Setting() {
                 <div className="flex flex-col">
                     {/* trick lỏ -mx-8 (kéo ra) và px-8 (đẩy chữ vào) */}
                     <div className="flex flex-col gap-1 border-b border-neutral-700 pb-4 -mx-8 px-8">
-                        <h1 className="text-3xl font-bold text-white">Cài đặt</h1>
-                        <h2 className="text-base text-neutral-400">Quản lý các tùy chọn cài đặt của bạn</h2>
+                        <h1 className="text-3xl font-bold text-white">{t('settings.title')}</h1>
+                        <h2 className="text-base text-neutral-400">{t('settings.description')}</h2>
                     </div>
                     <div className="flex flex-row gap-2 justify-between items-center mt-6">
                         <div className="flex flex-col gap-2">
-                            <h2 className="text-white text-xl font-bold">Ngôn ngữ</h2>
-                            <p className="text-neutral-300 text-base">Chọn ngôn ngữ - Có hiệu lực khi khởi động lại Website</p>
+                            <h2 className="text-white text-xl font-bold">{t('settings.language')}</h2>
+                            <p className="text-neutral-300 text-base">{t('settings.lang_desc')}</p>
                         </div>
                         <select 
                             className="bg-[#282828] text-white p-2 rounded-md border border-neutral-600 focus:ring-2 focus:ring-green-500 focus:border-green-500 h-10 w-40 cursor-pointer"
